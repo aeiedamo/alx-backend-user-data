e02001c03bc4cd8126c3a3c07631c1f6cedbc17d
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-08-15 18:36:47 +0100

    0x03 - test for #1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from pickle import NONE
+from sqlite3 import IntegrityError
+from requests import session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+            session_id (str, optional): The session ID of the user. Defaults to None.
+            reset_token (str, optional): The reset token of the user. Defaults to None.
+
+        Returns:
+            User: The newly created user object.
+
+        Raises:
+            ValueError: If the user cannot be added due to an IntegrityError.
+        """ 
+        try:
+            newUser = User(email=email, hashed_password=hashed_password)
+            self._session.add(newUser)
+            self._session.commit()
+            return newUser
+        except IntegrityError:
+            self._session.rollback()
+            raise ValueError("User with email {} already exists".format(email))
